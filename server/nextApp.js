3d3132334513f9ff627b80200abdf0bc2bb07366
Peter Garside-Quin <petegq@gmail.com>
2022-05-08 03:22:01 +0100

    Filter isFeatherService

--- server/nextApp.js
+++ server/nextApp.js
@@ -4,7 +4,14 @@ const dev = process.env.NODE_ENV !== "production";
 const nextApp = next({ dir: "./client", dev });
 const handle = nextApp.getRequestHandler();
 
+const feathersServices = ["/users"];
+
+const isFeathersService = path =>
+  feathersServices.some(item => path.indexOf(item) > -1);
+
+
 module.exports = {
   nextApp,
-  handle
+  handle,
+  isFeathersService
 };
